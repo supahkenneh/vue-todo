31af9ef29178c11bf24eabd4a88298f79775b194
supahkenneh <chunghlken@gmail.com>
2022-07-29 18:42:08 -0400

    add button

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -2,30 +2,32 @@
     <div v-if="!showForm">
         <Button btnLabel="Add Task" eventString="toggle-form" @toggle-form="showForm = !showForm" />
     </div>
-    <div v-else class="">
-        <form @submit.prevent="onSubmit">
-            <label for="task-name">
-                <input type="text" v-model="formTask">
-            </label>
-            <label for="priority">
-                <select name="priority" id="priority" v-model="formPriority">
-                    <option value="High">High</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Low">Low</option>
-                </select>
-            </label>
-            <label for="status">
-                <select name="status" id="status" v-model="formStatus">
-                    <option value="todo">To Do</option>
-                    <option value="progress">In Progress</option>
-                    <option value="done">Done</option>
-                </select>
-            </label>
-            <div>
-                <button>Submit</button>
-            </div>
-        </form>
-    </div>
+    <form v-else @submit.prevent="onSubmit" class="">
+        <div class="row mb-3">
+            <label for="task-name" class="form-label">Task</label>
+            <input type="text" class="form-control" v-model="formTask">
+        </div>
+        <div class="row mb-3">
+            <label for="priority" class="form-label">Priority</label>
+            <select name="priority" id="priority" class="form-select" v-model="formPriority">
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+            </select>
+        </div>
+        <div class="row mb-3">
+            <label for="status" class="form-label">Status</label>
+            <select name="status" id="status" class="form-select" v-model="formStatus">
+                <option value="todo">To Do</option>
+                <option value="progress">In Progress</option>
+                <option value="done">Done</option>
+            </select>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary">Submit</button>
+            <button type="button" class="btn btn-secondary">Close</button>
+        </div>
+    </form>
 </template>
 
 <script>
